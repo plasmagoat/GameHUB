87f3f3dfb18474815469682429c967541edd638b
David Mikael Blum Samuelsen <dbs@alpha-solutions.dk>
2019-02-21 10:35:18 +0100

    category view model and project structure

---
+++ GameHUB/Controllers/Commerce/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using GameHUB.Models.Commerce;
+using GameHUB.Models.ViewModels;
+
+namespace GameHUB.Controllers.Commerce
+{
+    public class CategoryController : ContentController<Category>
+    {
+        public ActionResult Index(Category currentContent)
+        {
+            /* Implementation of action. You can create your own view model class that you pass to the view or
+             * you can pass the page type for simpler templates */
+            var model = CategoryViewModel.Create(currentContent);
+            return View(model);
+        }
+    }
+}
---
+++ GameHUB/Models/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer;
+using EPiServer.Commerce.Catalog.ContentTypes;
+using EPiServer.ServiceLocation;
+
+namespace GameHUB.Models.ViewModels
+{
+    public class CategoryViewModel<TCategory> : IViewModel<TCategory> where TCategory : NodeContent
+    {
+        public CategoryViewModel(TCategory currentContent, IEnumerable<ProductContent> products)
+        {
+            CurrentContent = currentContent;
+            Products = products;
+        }
+        public TCategory CurrentContent { get; }
+        public LayoutModel Layout { get; set; }
+        public IEnumerable<ProductContent> Products { get; set; }
+        
+    }
+
+    public static class CategoryViewModel
+    {
+        private static Injected<IContentLoader> _contentLoader;
+        public static CategoryViewModel<TCategory> Create<TCategory>(TCategory category) where TCategory : NodeContent
+        {
+            var products = _contentLoader.Service.GetChildren<ProductContent>(category.ContentLink);
+            return new CategoryViewModel<TCategory>(category, products);
+        }
+    }
+
+
+}
